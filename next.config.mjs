2d86d0fe6a5b8ca10ac217a3e31073f2785d5c8f
md-fahad1 <20-43328-1@student.aiub.edu>
2025-07-10 01:37:03 +0600

    Fix: declare react-big-calendar for Vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "images.pexels.com" }],
   },
+  compilerOptions: {
+    typeRoots: ["./node_modules/@types", "./src/types"],
+  },
 };
 
 export default nextConfig;
